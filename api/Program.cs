5a366fd2544f613497e15ecd0f9f62b6f1a7f9d4
Weslley Alex <weslleyalexdasiuva@gmail.com>
2025-12-01 20:44:58 -0300

    adicionando front no cors

--- api/Program.cs
+++ api/Program.cs
@@ -14,7 +14,11 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
         policy => policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins(
+                "http://localhost:5173",
+                "https://localhost:5173",
+                "http://127.0.0.1:5173"
+            )
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials());
